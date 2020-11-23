f520a7f6b3d4742ba144b5d15121db8fb74f8e67
michael webster <michael.webster@motusdesign.ca>
2020-11-23 14:42:52 -0800

    sanity check

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const githubApi = require("./GithubGraphApi");
+const motus = require("./motus");
+const repoConfig = {
+      id : github.context.repo.id,
+      name : github.context.repo.name,
+      owner : github.context.repo.owner,
+      token : github.context.repo.token,
+    }
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const repoToken = core.getInput('repo-token');
-  console.log('bearer ',repoToken);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function action (){
+  try {
+    console.log(JSON.stringify(repoConfig));
+    const response = await githubApi.getRepoInfo(repoConfig);
+    console.log(JSON.stringify(response));
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+action();
