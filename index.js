2cf710bfde02d3c2f356a34e6361b816579e6eca
michael webster <michael.webster@motusdesign.ca>
2020-11-24 15:03:22 -0800

    step 4. columns

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function action (){
 
   try {
     console.log("config: " + JSON.stringify(repoConfig));
+    repoConfig.id = core.getInput('repo-id', {required: true});
     repoConfig.name = core.getInput('repo-name', {required: true});
     repoConfig.owner = core.getInput('repo-owner', {required: true});
     repoConfig.token = core.getInput('repo-token', {required: true});
@@ -25,7 +26,22 @@ async function action (){
 
     var issue = await githubApi.getIssueByNumber(repoConfig, issueNumber);
     console.log("Issue is:\n",JSON.stringify(issue));
-   
+    if (issue.projectCards.totalCount > 0) {
+      // - if card does not exist, throw error becasue issue is not assigned to a project
+      // @todo-later:
+      // - 2a: for each label throw error on incorrect design stage and state labels
+      // - mavenlink integration
+      const cards = issue.projectCards.nodes;
+      for(const card of cards){
+          const project = await githubApi.getProjectByNumber(repoInfo, card.project.number);
+          console.log("Card is: \n",JSON.stringify(card));
+          console.log("Project is: \n",JSON.stringify(project));
+          const column = await githubApi.getColumnByName(project.columns.nodes, stageLabel);
+          const check = await githubApi.setCardColumn(card.id, column.id);
+          console.log("Move result: ", JSON.stringify(check));
+      }
+    }  
+    // @todo: go through stage labels and remove other stages if they exist. 
     core.setOutput("repo", JSON.stringify(issue));
   } catch (error) {
     core.setFailed(error.message);
@@ -219,5 +235,5 @@ async function setup(){
   }
 }
 
-setup();
-// action();
+// setup();
+action();
