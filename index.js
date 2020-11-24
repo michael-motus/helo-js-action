df6cd653b81c3a664bcf6a1ce714d6e18303e089
michael webster <michael.webster@motusdesign.ca>
2020-11-23 20:01:55 -0800

    will this work?

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const githubApi = require('./GithubGraphApi');
 // const motus = require("./motus");
 var repoConfig = {
       id : null,
-      name : null,
-      owner : null,
+      // name : null,
+      // owner : null,
       token : github.context.token,
   // id: "MDEwOlJlcG9zaXRvcnkzMTQzOTQyNjY=",
-  // name: "helo-js-action",
-  // owner:  "michael-motus",
+  name: "helo-js-action",
+  owner:  "michael-motus",
   // token:"6dc0b11b5f007352b5ce99804c10ad56571c204d",
 }
 
 async function action (){
   try {
-    var repoString = github.context.repository;
-    repoString = repoString.split("/");
-    repoConfig.owner = repoString[0];
-    repoConfig.name = repoString[1];
+    // var repoString = github.context.repository;
+    // repoString = repoString.split("/");
+    // repoConfig.owner = repoString[0];
+    // repoConfig.name = repoString[1];
     
     console.log("context: " + JSON.stringify(github.context));
     console.log("config: " + JSON.stringify(repoConfig));
