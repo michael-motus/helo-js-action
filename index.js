769c5e5ac1171098678eff5b25993e93d1d747a1
michael webster <michael.webster@motusdesign.ca>
2020-11-24 13:14:25 -0800

    step 3.0 profit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const githubApi = require('./GithubGraphApi');
-// const motus = require("./motus");
+const motus = require("./motus");
 
 
-async function colunmUpdate (){
+async function action (){
   var repoConfig = {
     id: null,
     name: 'empty',
@@ -33,7 +33,49 @@ async function colunmUpdate (){
     core.setFailed(error.message);
   }
 }
-async function action(){
-  console.log("this shouldn't run");
+
+async function setup(){
+  const labels = [ ...motus.standards.disciplineLabels,
+      ...motus.standards.needLabels,
+      ...motus.standards.stageLabels,
+      ...motus.standards.typeLabels,
+  ];
+  const columns = motus.standards.stageColumns;
+  const projects = motus.standards.baseProjects;
+  var repoConfig = {
+    id: null,
+    name: 'empty',
+    owner: 'you',
+    token: null
+  };
+
+  try {
+    console.log("config: " + JSON.stringify(repoConfig));
+    repoConfig.name = core.getInput('repo-name', {required: true});
+    repoConfig.owner = core.getInput('repo-owner', {required: true});
+    repoConfig.token = core.getInput('repo-token', {required: true});
+    console.log("config: " + JSON.stringify(repoConfig));
+
+    for(const label of labels) {
+        console.log(repo_id, ", ", JSON.stringify(label));
+        const result = await githubApi.addRepoLabel(repo_id, label);
+        console.log("label result: ", JSON.stringify(result));
+    };
+    for(const project of projects) {
+        const resultProject = await githubApi.addRepoProject(repoConfig, project.name, project.body);
+        console.log("project result: ", JSON.stringify(resultProject));
+        var projID = resultProject.id;
+        console.log(projID);
+        // for(const column of columns) {
+        //     const resultColumn = await githubApi.addProjectColumn(resultProject.id, column.name)
+        //     console.log("project result: ", JSON.stringify(resultColumn));               
+        // }
+    }
+  } catch (err) {
+      console.log("failed", err.request)
+      console.log(err.message)
+  }
 }
-action();
+
+setup();
+// action();
