0542eaa6304cc833782d910fa8df45522e57e814
michael webster <michael.webster@motusdesign.ca>
2020-11-23 18:32:00 -0800

    fixing repo config

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const github = require('@actions/github');
 const githubApi = require('./GithubGraphApi');
 // const motus = require("./motus");
 const repoConfig = {
-//       id : github.context.repo.id,
-//       name : github.context.repo.name,
-//       owner : github.context.repo.owner,
-//       token : github.context.repo.token,
-  id: "MDEwOlJlcG9zaXRvcnkzMTQzOTQyNjY=",
-  name: "helo-js-action",
-  owner:  "michael-motus",
-  token:"6dc0b11b5f007352b5ce99804c10ad56571c204d",
+      id : github.context.repo.id,
+      name : github.context.repo.name,
+      owner : github.context.repo.owner,
+      token : github.context.repo.token,
+  // id: "MDEwOlJlcG9zaXRvcnkzMTQzOTQyNjY=",
+  // name: "helo-js-action",
+  // owner:  "michael-motus",
+  // token:"6dc0b11b5f007352b5ce99804c10ad56571c204d",
 }
 
 async function action (){
